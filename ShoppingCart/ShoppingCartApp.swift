ff2208e310ce5b6880558a03edbd7a2928d73130
C4sse <cassenhari@gmail.com>
2022-02-27 14:35:05 +0200

    Initial Commit

---
+++ ShoppingCart/ShoppingCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartApp.swift
+//  ShoppingCart
+//
+//  Created by George Nhari on 27/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
